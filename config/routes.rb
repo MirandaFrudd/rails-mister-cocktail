b7a6bd7d6a880235a10c03c4ca3eff0952b6be60
Miranda Frudd <mirandafrudd@outlook.com>
2021-02-04 17:28:47 +0000

    Created cards, added colours and rooted home

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'doses/new'
   # get 'doses/create'
-
+root to: 'cocktails#index'
+resources :cocktails, except: :index
   resources :cocktails, only: [:index, :new, :show, :create]do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
-  # resources :ingredients, only: [:index, :new, :create]do
-  #   resources :doses, only: [:new, :create, :destroy]
-  # end
-
-  # get '/cocktails', to: 'cocktails#index'
-  # get '/cocktails/new', to: 'cocktails#new'
-  # post '/cocktails', to: 'cocktails#create'
-  # get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
-  # get '/cocktails/:id/doses/new', to: 'doses#new'
 end
